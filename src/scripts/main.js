8c5d2f64051ccf7941f9b233e179528b60b0677c
Owanate <owanate999@gmail.com>
2024-02-26 18:41:24 +0100

    Add the JS fuctionality to the homepage

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,90 @@
 import $ from "jquery";
 
-$(".nav-toggle").click(() => {
+$(".nav-toggle").on("click", () => {
   $(".navbar").toggleClass("navbar--visible");
 });
+
+const Apikey = "LRdxrCLIL0c60Ckh4XHfuZkTAUHk3IfO";
+
+function formatDate(dateString) {
+  let dateObj = new Date(dateString);
+  let monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let dayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let dayOfWeek = dateObj.getDay();
+  let monthName = monthNames[dateObj.getMonth()];
+  let dayOfMonth = dateObj.getDate();
+  return dayNames[dayOfWeek] + ", " + monthName + " " + dayOfMonth;
+}
+
+function featuredEvents() {
+  try {
+    $.get(
+      `https://app.ticketmaster.com/discovery/v2/events.json?countryCode=BE&apikey=${Apikey}&size=6`,
+      (details) => {
+        $.each(details._embedded.events, (index, data) => {
+          let card = `
+            <li class="card flex">
+              <img src="${data.images[0].url}" alt=""/>
+              <div class="details flex">
+                <h3><a href="">${data.name}</a></h3>
+                <p class="date">${formatDate(data.dates.start.localDate)}</p>
+                <p class="venue">${data._embedded.venues[0].name}</p>
+              </div>
+            </li>
+          `;
+          $("#featured-events").append(card);
+        });
+      }
+    );
+  } catch (error) {
+    console.error("Fetch", error);
+  }
+}
+
+function attractions() {
+  try {
+    $.get(
+      `https://app.ticketmaster.com/discovery/v2/attractions.json?classificationName=museum&apikey=${Apikey}&size=3`,
+      (details) => {
+        $.each(details._embedded.attractions, (index, data) => {
+          let card = `
+            <li class="card flex">
+              <img src="${data.images[0].url}" alt=""/>
+              <div class="details flex">
+                <h3><a href="">${data.name}</a></h3>
+                <p class="date">${data.classifications[0].segment.name}</p>
+              </div>
+            </li>
+          `;
+          $("#attractions-grid").append(card);
+        });
+      }
+    );
+  } catch (error) {
+    console.error("Fetch", error);
+  }
+}
+
+featuredEvents();
+attractions();
